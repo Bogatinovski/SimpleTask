18f3c4a2d939a715b78ce584682c72637a7ee917
Dejan Bogatinovski <bogatinovski.dejan@gmail.com>
2018-03-25 18:06:57 +0200

    Create LocationsController with default Location CRUD operations

---
+++ SimpleTask/Controllers/LocationsController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SimpleTask.Models;
+
+namespace SimpleTask.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Locations")]
+    public class LocationsController : Controller
+    {
+        private readonly C3Context _context;
+
+        public LocationsController(C3Context context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Locations
+        [HttpGet]
+        public IEnumerable<Location> GetLocations()
+        {
+            return _context.Locations;
+        }
+
+        // GET: api/Locations/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetLocation([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var location = await _context.Locations.SingleOrDefaultAsync(m => m.Id == id);
+
+            if (location == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(location);
+        }
+
+        // PUT: api/Locations/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutLocation([FromRoute] int id, [FromBody] Location location)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != location.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(location).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!LocationExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Locations
+        [HttpPost]
+        public async Task<IActionResult> PostLocation([FromBody] Location location)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Locations.Add(location);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetLocation", new { id = location.Id }, location);
+        }
+
+        // DELETE: api/Locations/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteLocation([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var location = await _context.Locations.SingleOrDefaultAsync(m => m.Id == id);
+            if (location == null)
+            {
+                return NotFound();
+            }
+
+            _context.Locations.Remove(location);
+            await _context.SaveChangesAsync();
+
+            return Ok(location);
+        }
+
+        private bool LocationExists(int id)
+        {
+            return _context.Locations.Any(e => e.Id == id);
+        }
+    }
+}
